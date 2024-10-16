20847de97bd3708fa81e6de7d280ae63d203a030
min0622 <151921634+min0622@users.noreply.github.com>
2024-10-16 21:24:18 +0900

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
   // header_scrollfix
   let win = $(window);
@@ -6,85 +6,85 @@ $(function(){
   let section1 = $('#section1')
   let spot = section1.offset().top;
 
- win.scroll(function(){
-    
-  if(win.scrollTop()>spot){
-    header.addClass('fix')
-  } else{
-    header.removeClass('fix')
+  win.scroll(function () {
 
-  }
+    if (win.scrollTop() > spot) {
+      header.addClass('fix')
+    } else {
+      header.removeClass('fix')
+
+    }
 
   });
 
 
-// mo_side_menu
-$('.menu').click(function(){
-  $('.inner_side').slideToggle();
+  // mo_side_menu
+  $('.menu').click(function () {
+    $('#side_menu').slideToggle();
 
-  $('.top').toggleClass('open');
-  $('.mid').toggleClass('open');
-  $('.btm').toggleClass('open');
-
-
-  $('body').toggleClass('hidden')
-
-});
+    $('.top').toggleClass('open');
+    $('.mid').toggleClass('open');
+    $('.btm').toggleClass('open');
 
 
+    $('body').toggleClass('hidden')
 
-//main
-let currentIndex = 0;
-$('.slide_wrap').append($('.slide').first().clone(true));
-
-setInterval(function(){
-  currentIndex++;
-  
-  //슬라이드가 하나씩 이동
-  $('.slide_wrap').stop(true).animate({marginLeft:-currentIndex*100+"%"},600)
-
-  //슬라이드 처음으로 보내기
-  if(currentIndex==3){
-    setTimeout(function(){
-      $('.slide_wrap').stop(true).animate({marginLeft:'0'},0)
-
-      
-  currentIndex=0;
-    },700);
-  } 
+  });
 
 
-},3000);
+
+  //main
+  let currentIndex = 0;
+  $('.slide_wrap').append($('.slide').first().clone(true));
+
+  setInterval(function () {
+    currentIndex++;
+
+    //슬라이드가 하나씩 이동
+    $('.slide_wrap').stop(true).animate({ marginLeft: -currentIndex * 100 + "%" }, 600)
+
+    //슬라이드 처음으로 보내기
+    if (currentIndex == 3) {
+      setTimeout(function () {
+        $('.slide_wrap').stop(true).animate({ marginLeft: '0' }, 0)
+
+
+        currentIndex = 0;
+      }, 700);
+    }
+
+
+  }, 3000);
 
 
 
 
   // section2
-    var swiper = new Swiper(".mySwiper", {
-       
+  var swiper = new Swiper(".mySwiper", {
+
+    slidesPerView: 2,
+    loop: true,
+    spaceBetween: 10,
+    centeredSlides: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      576: {
         slidesPerView: 2,
-        loop: true,
-      spaceBetween: 10,
-      centeredSlides: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
+        spaceBetween: 16
       },
-        breakpoints: {
-            576: {
-                slidesPerView: 2,
-                spaceBetween: 16
-              },
-            860: {
-                slidesPerView: 3,
-                spaceBetween: 24
-              },
-            1400: {
-              slidesPerView:4, 
-              spaceBetween: 24,
-            },
-          },
-      });
+      860: {
+        slidesPerView: 3,
+        spaceBetween: 24
+      },
+      1400: {
+        slidesPerView: 4,
+        spaceBetween: 24,
+      },
+    },
+  });
 
 
 
